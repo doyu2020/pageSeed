96644c2c2fa3a152494ea8dc55d6600044e20ee7
doyu2020 <dybee89@gmail.com>
2022-04-29 07:49:02 +0800

    commit for mark

--- src/Render/Render.php
+++ src/Render/Render.php
@@ -3,15 +3,19 @@ declare(strict_types=1);
 
 namespace Dybee\PageSeed\Render;
 
-use Dybee\PageSeed\Middleware\RemoveComments;
+use Hyperf\Context\Context;
 use Hyperf\HttpMessage\Stream\SwooleStream;
 use Hyperf\View\Render as HyperfRender;
 use Psr\Http\Message\ResponseInterface;
 
 class Render extends HyperfRender
 {
+    /** @var string http mark key */
+    const MARK_NAME = '__HTTP_TEMPLATE_MARK';
+
     public function render(string $template, array $data = []): ResponseInterface
     {
+        $this->marks();
         $html = $this->getContents($template, $data);
         if (isset($this->config['pageSeed']['enable']) && $this->config['pageSeed']['enable']) {
             foreach ($this->config['pageSeed']['option'] as $option) {
@@ -21,6 +25,24 @@ class Render extends HyperfRender
         return $this->response()
             ->withAddedHeader('content-type', $this->getContentType())
             ->withBody(new SwooleStream($html));
+    }
+
+    /**
+     * set a mark for httpTemplate
+     * @return void
+     */
+    private function marks(): void
+    {
+        Context::set(self::MARK_NAME, true);
+    }
+
+    /**
+     * validation for mark
+     * @return bool
+     */
+    public function haskMark(): bool
+    {
+        return Context::get(self::MARK_NAME, false);
     }
 
 }
